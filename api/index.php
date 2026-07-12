24fca0577a57b4eca42390d487b5817d1bda4978
HENG  UCHIHAA <skimheng47@gmail.com>
2026-07-12 23:22:53 +0700

    Fix Target class view does not exist by redirecting bootstrap cache to tmp

--- api/index.php
+++ api/index.php
@@ -7,9 +7,17 @@ if (!is_dir($storage)) {
     mkdir($storage.'/framework/views', 0777, true);
     mkdir($storage.'/framework/sessions', 0777, true);
     mkdir($storage.'/logs', 0777, true);
+    mkdir($storage.'/bootstrap/cache', 0777, true);
 }
 $_ENV['LARAVEL_STORAGE_PATH'] = $storage;
 $_SERVER['LARAVEL_STORAGE_PATH'] = $storage;
+
+$cache = $storage.'/bootstrap/cache';
+$_ENV['APP_SERVICES_CACHE'] = $_SERVER['APP_SERVICES_CACHE'] = $cache.'/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $_SERVER['APP_PACKAGES_CACHE'] = $cache.'/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = $_SERVER['APP_CONFIG_CACHE'] = $cache.'/config.php';
+$_ENV['APP_ROUTES_CACHE'] = $_SERVER['APP_ROUTES_CACHE'] = $cache.'/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = $_SERVER['APP_EVENTS_CACHE'] = $cache.'/events.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
